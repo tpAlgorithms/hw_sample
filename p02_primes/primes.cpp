a045dca8557afe10f82c490a871cd92445b7a85e
Georgy Ivanov <ivanov.georg@gmail.com>
2015-09-26 10:25:05 +0300

    added prime generator

---
+++ p02_primes/primes.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+
+void make_primes(int max_prime, int *data) {
+  for (int pos = 0; pos < max_prime; ++pos) {
+    data[pos] = 1; 
+  }
+
+  for (int pos = 2; pos < max_prime; ++pos) {
+    if (data[pos] == 0) {
+      continue;
+    }
+    for (int candidate = pos * 2; candidate < max_prime; candidate += pos) {
+      printf("%d %d\n", pos, candidate);
+      data[candidate] = 0;
+    }
+  }
+}
+
+int main() {
+  int max_prime = 20;
+  int *data = new int[max_prime];
+  make_primes(max_prime, data);
+
+  for (int pos = 0; pos < max_prime; ++pos) {
+    if (data[pos] != 0) {
+      printf("%d ", pos);
+    }
+  }
+  delete [] data;
+
+  return 0;
+}
